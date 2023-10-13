18411facb4fff1b298ffb184d4e804c1854ab171
cjustinr <cjustinr12@gmail.com>
2023-10-14 02:09:17 +0800

    change delay and duration animation

--- components/sections/skill/Skill.jsx
+++ components/sections/skill/Skill.jsx
@@ -8,7 +8,7 @@ const Skill = ({ data = {} }) => {
         <motion.li
             initial={{ opacity: 0, translateY: 15 }}
             whileInView={{ opacity: 1, translateY: 0 }}
-            transition={{ delay: key * 0.3, duration: 0.5 }}
+            transition={{ delay: key * 0.2, duration: 0.3 }}
             viewport={{ once: true }}
             className="w-1/4 md:w-[100px] text-center flex flex-col justify-center items-center gap-1 grayscale-[1] hover:grayscale-0 hover:text-fern">
             <Image width={60} height={60} src={`/img/tools/${img}`} alt={title} className="select-none" />
